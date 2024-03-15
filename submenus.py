0663ca6a0be2062c03d0480667b36f2871fd841e
skostousov <93064193+skostousov@users.noreply.github.com>
2024-03-15 14:53:32 -0500

    deleted all extra phrases

--- submenus.py
+++ submenus.py
@@ -17,11 +17,7 @@ def authenticate(username, password):
 
   # Return True if the user exists and the password is correct, otherwise return False
   return user is not None
-print("made from my feature branch")
-print("another edit")
-print("final my feature")
-print("yet another edit")
-print("OObaloo")
+
 class MainMenu(Menu):
   def __init__(self, q, classname):
     self.choices = {"1": ("Sign In", self.signin), "2": ("Create Account", self.signup)}
