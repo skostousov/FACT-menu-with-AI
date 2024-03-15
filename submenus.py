395eccaf99eda8386cd44ede9b5581ac46f4b341
skostousov <93064193+skostousov@users.noreply.github.com>
2024-03-15 14:43:02 -0500

    Another New Feature

--- submenus.py
+++ submenus.py
@@ -18,6 +18,7 @@ def authenticate(username, password):
   # Return True if the user exists and the password is correct, otherwise return False
   return user is not None
 print("made from my feature branch")
+print("another edit")
 class MainMenu(Menu):
   def __init__(self, q, classname):
     self.choices = {"1": ("Sign In", self.signin), "2": ("Create Account", self.signup)}
